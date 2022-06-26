0045a4e1b27b3dda8848a4147bc844ce54a7fc9b
Mehedi Hasan <13490077+mhgitrepo@users.noreply.github.com>
2022-06-26 12:06:25 +0600

    interesting react routes, 404-page setup

---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Hello Friend. How are you!</h2>
+            <p>Please, if possible lend me some money!! :3</p>
+        </div>
+    );
+};
+
+export default Friends;
