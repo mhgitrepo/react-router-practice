6b602b04272219df0fac20942be9dba983cce226
Mehedi Hasan <13490077+mhgitrepo@users.noreply.github.com>
2022-06-26 12:43:10 +0600

    loading friends from friend route

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Friend = (props) => {
-    const {name, username} = props.friend;
+    const {name, username, address} = props.friend;
+    const getAddress = Object.values(address);
+    console.log(getAddress);
     return (
         <div>
             <h2>Name: {name}</h2>
             <button>{username}</button>
+            <p>{getAddress}</p>
         </div>
     );
 };
