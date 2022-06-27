f121bde1b1084b09ac8c26b1b00417a75ff21874
Mehedi Hasan <13490077+mhgitrepo@users.noreply.github.com>
2022-06-27 15:21:35 +0600

    dynamic friend name from friendDetail component

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
-    // const params = useParams();
     const {friendId} = useParams();
+    const [friend, setFriend] = useState({});
+
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setFriend(data))
+    }, [])
+
     return (
-        <div>
-            <h2>This is Detail about a Friend: {friendId}</h2>
+        <div style={{border: '2px solid salmon', borderRadius: '8px', margin: '2em 15em 2em 15em'}}>
+            <h2 style={{border: '2px solid green', borderRadius: '8px', margin: '1em 1em 0em 1em'}}>This is Detail about a Friend: {friendId}</h2>
+            <h3>Name: {friend.name}</h3>
+            <h4>Email: {friend.email}</h4>
+            <h5>Website: {friend.website}</h5>
+            <p>City: {friend.address?.city}</p>
+            <p>Lat: {friend.address?.geo?.lat}</p>
+            <p>Lng: {friend.address?.geo?.lng}</p>
         </div>
     );
 };
